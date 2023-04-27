b4f1b96a6b213070b08b3784824bb3f2796dbed4
Premankur <premankur.qb9@gmail.com>
2023-04-27 07:05:30 +0530

    script to change size and policy

---
+++ ChampSim/cache_size.sh
@@ -0,0 +1,50 @@
+# l1d,l2c,llc number of sets will be changed with this script
+
+if [[ $4 != 1 && $4 != 2 && $4 != 3 ]]; then
+    echo "Inclusive/Exclusive/Non-Inclusive for 1,2,3 only"
+    exit 1
+fi
+
+while read line; do
+  if [[ $line == *"#define L1D_SET"* ]]; then
+    echo "#define L1D_SET $1"
+  else
+    echo "$line"
+  fi
+done < inc/cache.h > inc/cache2.h
+
+rm inc/cache.h
+
+
+while read line; do
+  if [[ $line == *"#define L2C_SET"* ]]; then
+    echo "#define L2C_SET $2"
+  else
+    echo "$line"
+  fi
+done < inc/cache2.h > inc/cache3.h
+
+rm inc/cache2.h
+
+
+while read line; do
+  if [[ $line == *"#define LLC_SET NUM_CPUS"* ]]; then
+    echo "#define LLC_SET NUM_CPUS*$3"
+  else
+    echo "$line"
+  fi
+done < inc/cache3.h > inc/cache4.h
+
+rm inc/cache3.h
+
+while read line; do
+  if [[ $line == *"#define INC_POLICY"* ]]; then
+    echo "#define INC_POLICY $4"
+  else
+    echo "$line"
+  fi
+done < inc/cache4.h > inc/cache.h
+
+rm inc/cache4.h
+
+
